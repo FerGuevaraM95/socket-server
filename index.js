6fd02ec55175c85959e19db90e8d218f69c563a0
Fernando Guevara <luisfernandoguevara95@gmail.com>
2020-11-24 21:12:23 -0600

    (Add): Socket io server

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Servidor de express
-const app = require('express')();
+const express = require('express');
+const app = express();
 
 // Servidor de sockets
 const server = require('http').createServer(app);
@@ -7,8 +8,19 @@ const server = require('http').createServer(app);
 // Configuración del socket server
 const io = require('socket.io')(server);
 
+// Desplegar el directorio público
+app.use(express.static(__dirname + '/public'))
 
-io.on('connection', () => { /* … */ });
+io.on('connection', (socket) => {
+  
+  socket.emit('mensaje-bienvendida', {
+    msg: "bienvenido al server",
+    date: new Date()
+  })
+
+});
+
+
 server.listen(8080, () => {
   console.log('servidor corriendo en el puerto :8080');
 });
